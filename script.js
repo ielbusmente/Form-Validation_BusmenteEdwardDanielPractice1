25c7fa606427596ca263a7c4c594064fbc94e7a2
Edward Daniel J. Busmente <2018112067@ust-ics.mygbiz.com>
2020-02-16 16:12:10 +0800

    validation changes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var events = document.getElementById('events').value;
-var eventT = document.getElementById('EventTitle').value;
+// var events = document.getElementById('events').value;
+// var eventT = document.getElementById('EventTitle').value;
 var sDate = document.getElementById('EventDate').value;
 var eDate = document.getElementById('to').value;
 var pDate = document.getElementById('PostingDate').value;
@@ -8,19 +8,16 @@ var pDate = document.getElementById('PostingDate').value;
 
 function validate(){
  
-    if (events === "") {
-        alert("An Event Type must be chosen.");
-        return false;
-    }
-    else if (eventT === "") {
-        alert("Please fill up Event Title.");
-        return false;
-    } 
-    else if (eventT === "") {
-        alert("Please fill up Event Title.");
-        return false;
-    } 
-    else if (sDate > eDate) {
+    // if (events === "") {
+    //     alert("An Event Type must be chosen.");
+    //     return false;
+    // }
+    // else if (eventT === "") {
+    //     alert("Please fill up Event Title.");
+    //     return false;
+    // }  
+    // else 
+    if (sDate > eDate) {
         alert("Starting date must be before the finishing date.");
         return false;
     } 
