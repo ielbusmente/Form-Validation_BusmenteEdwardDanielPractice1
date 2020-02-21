86a2233be8e89f6ee0578b84de7a9ccc69395aa7
maclab <2018112067@ust-ics.mygbiz.com>
2020-02-21 15:44:24 +0800

    try catch

--- UserPass/js.js
+++ UserPass/js.js
@@ -1,26 +1,13 @@
-class person {
-    constructor(name, gender) {
-        this.name = name;
-        this.gender = gender;
-    }
-    walk(distance) {
-        alert(`${this.name} walks ${distance} miles per day.`);
-    }
+
+var sayHello = function() {
+ 
 }
 
-class stud extends person {
-    constructor(name, gender, course){
-        super(name, gender);
-        this.course = course;
-    }
-    study(sub) {
-        alert(`${this.name} is enrolled in ${sub}.`);
-    }
+try {
+    sayHello();
+}catch(e) {
+    console.log("An error has occured...");
+}finally{
+    console.log("Finally, block has executed...");
 }
 
-var std1 = new stud("Daniel", "Male", "IT");
-var p1 = new person("Joyce", "Female");
-console.log(typeof(p1));
-p1 = 3;
-console.log(typeof(p1));
-//polymorph ability of an object to take on many forms. The most common use of polymorphism in OOP occurs when a parent class reference is used to refer to a child class object. 
