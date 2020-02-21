68441b35cde20b70101abb57e67074d18be290bd
Karl <Karladrian.solmiano.iics@ust.edu.ph>
2020-02-21 10:58:57 +0800

    11am

--- UserPass/script.js
+++ UserPass/script.js
@@ -1,15 +1,23 @@
 function passValidation() { 
+    var red = "rgb(255, 130, 130)";
+    var green = "rgb(142, 255, 151)";
     var pass = document.getElementById('pass');
+    var confPass = document.getElementById('confPass');
     var status = document.getElementById('stat');
     var cou = 0;
 
-    if (pass.value == "") {
-        pass.style.background = "rgb(255, 130, 130)";
+    if (pass.value == "" ) {
+        pass.style.background = red;
     }
+    if (confPass.value == "" ) {
+        confPass.style.background = red;
+    } 
+    if (pass.value != confPass.value) {
+        pass.style.background = red;
+        confPass.style.background = red;  
+    } 
+    
 
-    else  {
-        pass.style.background = "rgb(142, 255, 151)";
-    }
 }
 
 function submitValidation() {
